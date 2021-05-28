a32b1f2c972cbc3e5468e7d8ca15cc932a432750
caozhiyi <272653256@qq.com>
2021-05-28 10:07:13 +0800

    modify mac kqueue action.

--- cppnet/event/mac/kqueue_action.h
+++ cppnet/event/mac/kqueue_action.h
@@ -23,14 +23,13 @@ public:
     virtual bool Init(uint32_t thread_num = 0);
     virtual bool Dealloc();
     // net io event
-    virtual bool AddSendEvent(std::shared_ptr<Event>& event);
-    virtual bool AddRecvEvent(std::shared_ptr<Event>& event);
-    virtual bool AddAcceptEvent(std::shared_ptr<Event>& event);
+    virtual bool AddSendEvent(Event* event);
+    virtual bool AddRecvEvent(Event* event);
+    virtual bool AddAcceptEvent(Event* event);
+    virtual bool AddConnection(Event* event, Address& address);
+    virtual bool AddDisconnection(Event* event);
 
-    virtual bool AddConnection(std::shared_ptr<Event>& event, Address& address);
-    virtual bool AddDisconnection(std::shared_ptr<Event>& event);
-
-    virtual bool DelEvent(std::shared_ptr<Event>& event);
+    virtual bool DelEvent(Event* event);
     // io thread process
     virtual void ProcessEvent(int32_t wait_ms);
     // weak up net io thread
