9e44528e1cb180382789623423717d2a02104476
caozhiyi <272653256@qq.com>
2021-07-20 19:22:02 +0800

    format code.

--- common/log/base_logger.h
+++ common/log/base_logger.h
@@ -2,16 +2,17 @@
 // that can be found in the LICENSE file.
 
 // Author: caozhiyi (caozhiyi5@gmail.com)
+// Copyright <caozhiyi>
 
-#ifndef QUIC_COMMON_LOG_BASE_LOGGER
-#define QUIC_COMMON_LOG_BASE_LOGGER
+#ifndef COMMON_LOG_BASE_LOGGER_H_
+#define COMMON_LOG_BASE_LOGGER_H_
 
 #include <memory>
 #include <cstdint>
-#include <stdarg.h>
+#include <cstdarg>
 
-#include "log.h"
-#include "log_stream.h"
+#include "common/log/log.h"
+#include "common/log/log_stream.h"
 #include "common/structure/thread_safe_queue.h"
 
 namespace cppnet {
@@ -21,37 +22,43 @@ struct Log;
 class Logger;
 class Alloter;
 class BaseLogger {
-public:
-    BaseLogger(uint16_t cache_size, uint16_t block_size);
-    ~BaseLogger();
+ public:
+  BaseLogger(uint16_t cache_size, uint16_t block_size);
+  ~BaseLogger();
 
-    void SetLogger(std::shared_ptr<Logger> log) { _logger = log; }
+  void SetLogger(std::shared_ptr<Logger> log) { _logger = log; }
 
-    void SetLevel(LogLevel level);
+  void SetLevel(LogLevel level);
 
-    void Debug(const char* file, uint32_t line, const char* content, va_list list);
-    void Info(const char* file, uint32_t line, const char* content, va_list list);
-    void Warn(const char* file, uint32_t line, const char* content, va_list list);
-    void Error(const char* file, uint32_t line, const char* content, va_list list);
-    void Fatal(const char* file, uint32_t line, const char* content, va_list list);
+  void Debug(const char* file, uint32_t line,
+    const char* content, va_list list);
+  void Info(const char* file, uint32_t line,
+    const char* content, va_list list);
+  void Warn(const char* file, uint32_t line,
+    const char* content, va_list list);
+  void Error(const char* file, uint32_t line,
+    const char* content, va_list list);
+  void Fatal(const char* file, uint32_t line,
+    const char* content, va_list list);
 
-    LogStreamParam GetStreamParam(LogLevel level, const char* file, uint32_t line);
+  LogStreamParam GetStreamParam(LogLevel level,
+    const char* file, uint32_t line);
 
-private:
-    std::shared_ptr<Log> GetLog();
-    void FreeLog(Log* log);
-    Log* NewLog();
+ private:
+  std::shared_ptr<Log> GetLog();
+  void FreeLog(Log* log);
+  Log* NewLog();
 
-protected:
-    uint16_t _level;
-    uint16_t _cache_size;
-    uint16_t _block_size;
+ protected:
+  uint16_t _level;
+  uint16_t _cache_size;
+  uint16_t _block_size;
 
-    std::shared_ptr<Alloter> _allocter;
-    ThreadSafeQueue<Log*>    _cache_queue;
-    std::shared_ptr<Logger>  _logger;
+  std::shared_ptr<Alloter> _allocter;
+  ThreadSafeQueue<Log*>    _cache_queue;
+  std::shared_ptr<Logger>  _logger;
 };
 
-}
+}  // namespace cppnet
 
-#endif
+#endif  // COMMON_LOG_BASE_LOGGER_H_
